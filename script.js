e727775cbc3e0661674d40bf377626ec7e7901ea
Dawn Best <55746955+dawnbest@users.noreply.github.com>
2020-02-11 10:07:31 -0500

    Uploaded new version of script
    New version of script JS

--- script.js
+++ script.js
@@ -2,27 +2,22 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below
-function generateTarget() {
-  return Math.floor(Math.random());
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
 }
 
-function compareGuesses(humGuess, comGuess, num) {
-  if (humGuess === num) {
-    return true
-  } else {
-    return false
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+  const humanDifference = Math.abs(targetGuess - humanGuess)
+  const computerDifference = Math.abs(targetGuess - computerGuess)
+  return humanDifference <= computerDifference;
+}
+
+const updateScore = winner => {
+  if (winner === 'human') {
+    humanScore++;
+  } else if (winner === 'computer') {
+    computerScore++;
   }
 }
 
-function updateScore(winner) {
-  if (winner === 'human') {
-      humanScore += 1;
-      } else {
-        computerScore += 1;
-      }
-}
-
-function advanceRound() {
-  currentRoundNumber += 1;
-}
+const advanceRound = () => currentRoundNumber++;
